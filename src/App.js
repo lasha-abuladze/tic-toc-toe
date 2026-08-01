562a6f72679bd16290f9f58f041aa1745f39ac4e
Lasha Abuladze <lashaa.abuladzee@gmail.com>
2026-08-01 05:57:53 +0400

    feat: complete new game menu UI and state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logoImg from "./assets/logo.svg";
 import { ReactComponent as IconO } from "./assets/icon-o.svg";
 import { ReactComponent as IconX } from "./assets/icon-x.svg";
+import { useState } from "react";
 
 export default function App() {
   return (
@@ -11,12 +12,27 @@ export default function App() {
 }
 
 function NewGameMenu() {
+  const [isActive, setIsActive] = useState(false);
+
+  function handleActiveMark() {
+    setIsActive(!isActive);
+  }
+
   return (
     <div className="new-game-menu">
       <Logo />
-      <PlayersMarkContainer />
-      <BtnNewGame btnText={`NEW GAME (VS CPU)`} />
-      <BtnNewGame btnText={`NEW GAME  (VS PLAYER)`} />
+      <PlayersMarkContainer
+        handleActiveMark={handleActiveMark}
+        isActive={isActive}
+      />
+      <BtnNewGame
+        className="btn--gen btn--chose-opponent btn--chose-opponent--vs-cpu"
+        btnText={`NEW GAME (VS CPU)`}
+      />
+      <BtnNewGame
+        className="btn--gen btn--chose-opponent btn--chose-opponent--vs-player"
+        btnText={`NEW GAME  (VS PLAYER)`}
+      />
     </div>
   );
 }
@@ -25,23 +41,43 @@ function Logo() {
   return <img className="logo" src={logoImg} alt="logo" />;
 }
 
-function PlayersMarkContainer() {
+function PlayersMarkContainer({ handleActiveMark, isActive }) {
   return (
     <div className="players-mark-container">
       <p>PICK PLAYER 1’S MARK</p>
       <div className="marks">
-        <BtnMark mark={<IconX className="icon" />} />
-        <BtnMark mark={<IconO />} />
+        <BtnMark
+          handleActiveMark={handleActiveMark}
+          className={
+            isActive
+              ? `btn--gen btn--mark-icon`
+              : `btn--gen btn--mark-icon mark-active`
+          }
+          mark={<IconX className="icon-mark iconX-mark" />}
+        />
+        <BtnMark
+          handleActiveMark={handleActiveMark}
+          className={
+            !isActive
+              ? `btn--gen btn--mark-icon`
+              : `btn--gen btn--mark-icon mark-active`
+          }
+          mark={<IconO className="icon-mark iconO-mark" />}
+        />
       </div>
       <p>REMEMBER : X GOES FIRST</p>
     </div>
   );
 }
 
-function BtnMark({ mark }) {
-  return <button className="icon">{mark}</button>;
+function BtnMark({ mark, className, handleActiveMark }) {
+  return (
+    <button onClick={handleActiveMark} className={className}>
+      {mark}
+    </button>
+  );
 }
 
-function BtnNewGame({ btnText }) {
-  return <button>{btnText}</button>;
+function BtnNewGame({ btnText, className }) {
+  return <button className={className}>{btnText}</button>;
 }
