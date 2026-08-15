55654214a358df258bf979d6ca4225e7c43ab79f
Lasha Abuladze <lashaa.abuladzee@gmail.com>
2026-08-15 12:53:26 +0400

    1

--- src/App.js
+++ src/App.js
@@ -4,25 +4,47 @@ import { ReactComponent as IconX } from "./assets/icon-x.svg";
 import { useState } from "react";
 
 export default function App() {
+  const [chosenMark, setChosenMark] = useState(`x`);
+  const [chooseOpponent, setChooseOpponent] = useState();
+  // let chosenMark = `x`;
+  // const [playWith, setPlayWith] = useState();
+  // let playWith;
+
+  function startGame() {
+    console.log(
+      `you have chossen ${chosenMark} and play with ${chooseOpponent}`,
+    );
+  }
+
   return (
     <>
-      <NewGameMenu />
+      <NewGameMenu
+        chosenMark={setChosenMark}
+        playWith={setChooseOpponent}
+        startGame={startGame}
+      />
     </>
   );
 }
 
-function NewGameMenu() {
+function NewGameMenu({ chosenMark, playWith, startGame }) {
   return (
     <div className="new-game-menu">
       <Logo />
-      <PlayersMarkContainer />
+      <PlayersMarkContainer chosenMark={chosenMark} />
       <BtnNewGame
         className="btn--gen btn--chose-opponent btn--chose-opponent--vs-cpu"
         btnText={`NEW GAME (VS CPU)`}
+        dataPlayWith="cpu"
+        playWith={playWith}
+        startGame={startGame}
       />
       <BtnNewGame
         className="btn--gen btn--chose-opponent btn--chose-opponent--vs-player"
         btnText={`NEW GAME  (VS PLAYER)`}
+        dataPlayWith="player"
+        playWith={playWith}
+        startGame={startGame}
       />
     </div>
   );
@@ -32,15 +54,19 @@ function Logo() {
   return <img className="logo" src={logoImg} alt="logo" />;
 }
 
-function PlayersMarkContainer() {
+function PlayersMarkContainer({ chosenMark }) {
   const [isActive, setIsActive] = useState(true);
 
-  function active() {
+  function active(el) {
     setIsActive(true);
+    chosenMark(el.dataset.mark);
+    // console.log(chosenMark);
   }
 
-  function noActive() {
+  function noActive(el) {
     setIsActive(false);
+    chosenMark(el.dataset.mark);
+    // console.log(chosenMark);
   }
 
   return (
@@ -54,7 +80,8 @@ function PlayersMarkContainer() {
               : `btn--gen btn--mark-icon`
           }
           mark={<IconX className="icon-mark iconX-mark" />}
-          onClick={active}
+          onClick={(e) => active(e.target.closest(`.btn--mark-icon`))}
+          dataMark="x"
         />
         <BtnMark
           className={
@@ -63,7 +90,8 @@ function PlayersMarkContainer() {
               : `btn--gen btn--mark-icon`
           }
           mark={<IconO className="icon-mark iconO-mark" />}
-          onClick={noActive}
+          onClick={(e) => noActive(e.target.closest(`.btn--mark-icon`))}
+          dataMark="o"
         />
       </div>
       <p>REMEMBER : X GOES FIRST</p>
@@ -71,14 +99,29 @@ function PlayersMarkContainer() {
   );
 }
 
-function BtnMark({ mark, className, onClick }) {
+function BtnMark({ mark, className, onClick, dataMark }) {
   return (
-    <button onClick={onClick} className={className}>
+    <button onClick={onClick} className={className} data-mark={dataMark}>
       {mark}
     </button>
   );
 }
 
-function BtnNewGame({ btnText, className }) {
-  return <button className={className}>{btnText}</button>;
+function BtnNewGame({ btnText, className, dataPlayWith, playWith, startGame }) {
+  function chooseOpponent(el) {
+    playWith(el.target.dataset.playWith);
+  }
+
+  return (
+    <button
+      onClick={(e) => {
+        chooseOpponent(e);
+        startGame();
+      }}
+      className={className}
+      data-play-with={dataPlayWith}
+    >
+      {btnText}
+    </button>
+  );
 }
